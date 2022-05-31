fe838e3d4b2d37dc5cbe321a68eee0bfb5ebcc52
Hannah Niese <43292293+hannahniese@users.noreply.github.com>
2022-05-31 15:45:53 -0700

    new measurements

--- readout_multimeter_v03.py
+++ readout_multimeter_v03.py
@@ -30,29 +30,45 @@ v34401A = rm.open_resource('GPIB0::22::INSTR')
 # open file to write data
 file = open(fileNameString, 'w')
 
+
 # Test for connection by asking for identification 
 ReturnValue = v34401A.query('*IDN?')
 
 # scan through spatial locations
 motor = MicroDrive()
-acq     = 150
+acq     = 50
+rows    = 50
 t       = 0
+p       = 0
+xmem    = 0
+ymem    = 0
+
+
+file.write('No of points in x' + str(acq) + 'no of points in y' + str(rows) + '\n')
+
 while t < acq:
-    # stage control
     t += 1
     x = -0.1
-    y = 0
-    z = 0
-    print(x,y,z)
-    motor.move(x,y,z)
-    
-    # acquisition control
-    temp_values = v34401A.query_ascii_values(':MEASure:VOLTage:DC? %s,%s' % ('MIN', 'MAX'))
-    measuredValue = temp_values[0]
-    print('Voltage:  {0}'.format(str(measuredValue))) # Commenting out prints increases acquistion speed
-    acquisitionArray.append(t)        # Store our data in a local array
-    dataArray.append(float(measuredValue))           # 
-    file.write(str(x) + ', ' + str(y) + ', ' + str(z) + ', ' + '{0}\n'.format(str(measuredValue)))
+    xmem = xmem + x
+    while p < rows:
+        # stage control
+        p += 1
+        y = -0.1
+        ymem = ymem + y
+        z = 0
+        print(x,y,z)
+        motor.move(x,y,z)
+        
+        # acquisition control
+        temp_values = v34401A.query_ascii_values(':MEASure:VOLTage:DC? %s,%s' % ('MIN', 'MAX'))
+        measuredValue = temp_values[0]
+        print('Voltage:  {0}'.format(str(measuredValue)))   # Commenting out prints increases acquistion speed
+        print('p =' + str(p) + ', t =' + str(t))              # print progress
+        acquisitionArray.append(t)        # Store our data in a local array
+        dataArray.append(float(measuredValue))           # 
+        file.write(str(xmem) + ', ' + str(ymem) + ', ' + str(z) + ', ' + '{0}\n'.format(str(measuredValue)))
+    motor.move(0,-p*y,0)
+    ymem = 0
 v34401A.close()    # Close our connection to the instrument
 rm.close()
 
