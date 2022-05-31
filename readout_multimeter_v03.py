fb72c19c5b6d2ae6d2b7353897b0eab4b9d04a45
Hannah Niese <43292293+hannahniese@users.noreply.github.com>
2022-05-31 16:16:02 -0700

    new data

--- readout_multimeter_v03.py
+++ readout_multimeter_v03.py
@@ -10,6 +10,7 @@ import time
 import csv
 import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Generate the name of the data file based on the start of the script
 dataTimeString = datetime.datetime.now().strftime("%y%m%d%H%M%S")
@@ -36,39 +37,46 @@ ReturnValue = v34401A.query('*IDN?')
 
 # scan through spatial locations
 motor = MicroDrive()
-acq     = 50
-rows    = 50
+acq     = 8
+rows    = 8
 t       = 0
 p       = 0
 xmem    = 0
 ymem    = 0
+counter = 0
 
 
 file.write('No of points in x' + str(acq) + 'no of points in y' + str(rows) + '\n')
 
 while t < acq:
     t += 1
-    x = -0.1
+    x = 0
     xmem = xmem + x
     while p < rows:
         # stage control
         p += 1
-        y = -0.1
+        y = 0.2
         ymem = ymem + y
         z = 0
         print(x,y,z)
         motor.move(x,y,z)
+        counter += 1
         
         # acquisition control
         temp_values = v34401A.query_ascii_values(':MEASure:VOLTage:DC? %s,%s' % ('MIN', 'MAX'))
         measuredValue = temp_values[0]
         print('Voltage:  {0}'.format(str(measuredValue)))   # Commenting out prints increases acquistion speed
-        print('p =' + str(p) + ', t =' + str(t))              # print progress
+        print('p = ' + str(p) + ', t = ' + str(t) + ', acq ' + str(counter) + '/' + str(acq*rows))
+        print('x = ' + str(xmem) + ', y = ' + str(ymem))              # print progress
         acquisitionArray.append(t)        # Store our data in a local array
+        coord_x.append(t)                 # Store our data in a local array
+        coord_y.append(p)                 # Store our data in a local array
         dataArray.append(float(measuredValue))           # 
         file.write(str(xmem) + ', ' + str(ymem) + ', ' + str(z) + ', ' + '{0}\n'.format(str(measuredValue)))
-    motor.move(0,-p*y,0)
+    motor.move(0,-rows*y,0)
+    x = x + 0.2
     ymem = 0
+    p = 0
 v34401A.close()    # Close our connection to the instrument
 rm.close()
 
@@ -77,8 +85,36 @@ rm.close()
 plt.xlabel('x-coordinate')
 plt.ylabel('Voltage')
 plt.title('HP 34401A Data')
-plt.plot(acquisitionArray,dataArray)
+plt.plot(coord_x,dataArray)
 plt.savefig(figureNameString)
+plt.show()
+
+#%% plot 2D heatmap
+
+array = np.zeros((100,4))
+array[:,0] = coord_x
+array[:,1] = coord_y
+#array[:,2] = coord_z
+array[:,3] = dataArray
+
+# importing mplot3d toolkits, numpy and matplotlib
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+fig = plt.figure()
+ 
+# syntax for 3-D projection
+ax = plt.axes(projection ='3d')
+ 
+# defining all 3 axes
+z = dataArray
+x = coord_x
+y = coord_y
+ 
+# plotting
+ax.plot3D(x, y, z, 'green')
+ax.set_title('3D line plot geeks for geeks')
 plt.show()
 
     
