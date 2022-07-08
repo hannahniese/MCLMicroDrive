6d3e3886eeb5c237f1337dc1cd3dbc0c1702d66c
Hannah Niese <43292293+hannahniese@users.noreply.github.com>
2022-07-08 16:33:52 -0700

    interpolation implemented

--- Dataanalysis_v06.py
+++ Dataanalysis_v06.py
@@ -41,9 +41,9 @@ I_side = side_data[:,7]*1000
 # calculate range of Intensitiy for normalization
 delta_I     =   I_side.max()-I_side.min()
 
-x = data[:,4]
-y = data[:,5]
-z = data[:,6]
+x = data[:,1]
+y = data[:,2]
+z = data[:,3]
 I = data[:,7]*1000 # convert to mV
 
 
@@ -139,3 +139,109 @@ plt.show()
 
 os.chdir(outpath)
 plt.savefig('%s_values_1.png' % file, dpi=600)
+
+#%% Meshgrid trial
+import scipy as sc
+
+def griddata(x, y, z, binsize=0.0015, retbin=True, retloc=True):
+    """
+    Place unevenly spaced 2D data on a grid by 2D binning (nearest
+    neighbor interpolation).
+    
+    Parameters
+    ----------
+    x : ndarray (1D)
+        The idependent data x-axis of the grid.
+    y : ndarray (1D)
+        The idependent data y-axis of the grid.
+    z : ndarray (1D)
+        The dependent data in the form z = f(x,y).
+    binsize : scalar, optional
+        The full width and height of each bin on the grid.  If each
+        bin is a cube, then this is the x and y dimension.  This is
+        the step in both directions, x and y. Defaults to 0.01.
+    retbin : boolean, optional
+        Function returns `bins` variable (see below for description)
+        if set to True.  Defaults to True.
+    retloc : boolean, optional
+        Function returns `wherebins` variable (see below for description)
+        if set to True.  Defaults to True.
+   
+    Returns
+    -------
+    grid : ndarray (2D)
+        The evenly gridded data.  The value of each cell is the median
+        value of the contents of the bin.
+    bins : ndarray (2D)
+        A grid the same shape as `grid`, except the value of each cell
+        is the number of points in that bin.  Returns only if
+        `retbin` is set to True.
+    wherebin : list (2D)
+        A 2D list the same shape as `grid` and `bins` where each cell
+        contains the indicies of `z` which contain the values stored
+        in the particular bin.
+
+    Revisions
+    ---------
+    2010-07-11  ccampo  Initial version
+    """
+    # get extrema values.
+    xmin, xmax = x.min(), x.max()
+    ymin, ymax = y.min(), y.max()
+
+    # make coordinate arrays.
+    xi      = np.arange(xmin, xmax+binsize, binsize)
+    yi      = np.arange(ymin, ymax+binsize, binsize)
+    xi, yi  = np.meshgrid(xi,yi)
+
+    # make the grid.
+    grid           = np.zeros(xi.shape, dtype=x.dtype)
+    nrow, ncol = grid.shape
+    if retbin: bins = np.copy(grid)
+
+    # create list in same shape as grid to store indices
+    if retloc:
+        wherebin = np.copy(grid)
+        wherebin = wherebin.tolist()
+
+    # fill in the grid.
+    for row in range(nrow):
+        for col in range(ncol):
+            xc = xi[row, col]    # x coordinate.
+            yc = yi[row, col]    # y coordinate.
+
+            # find the position that xc and yc correspond to.
+            posx = np.abs(x - xc)
+            posy = np.abs(y - yc)
+            ibin = np.logical_and(posx < binsize/2., posy < binsize/2.)
+            ind  = np.where(ibin == True)[0]
+
+            # fill the bin.
+            bin = z[ibin]
+            if retloc: wherebin[row][col] = ind
+            if retbin: bins[row, col] = bin.size
+            if bin.size != 0:
+                binval         = np.median(bin)
+                grid[row, col] = binval
+            else:
+                grid[row, col] = np.nan   # fill empty bins with nans.
+
+    # return the grid
+    if retbin:
+        if retloc:
+            return grid, bins, wherebin, xi, yi
+        else:
+            return grid, bins
+    else:
+        if retloc:
+            return grid, wherebin
+        else:
+            return grid
+
+
+grid, bins, wherebin, xi, yi = griddata(x, y, I, binsize=0.0015, retbin=True, retloc=True)
+
+interpolatedgrid = sc.interpolate.interp2d(xi, yi, grid)
+
+
+plt.imshow(interpolatedgrid)
